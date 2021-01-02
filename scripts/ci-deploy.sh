aedecae0feae147d3ffdb84f845bfb3e6f93fb95
mohammed hussein <mhussein666666@gmail.com>
2021-01-02 14:24:09 +0000

    add deploy script and add new steps to circleci config

--- scripts/ci-deploy.sh
+++ scripts/ci-deploy.sh
@@ -16,7 +16,7 @@ mv ./kube/do-sample-deployment.yml.out ./kube/do-sample-deployment.yml
 echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
 
 ./kubectl \
-  --kubeconfig=/dev/null \
+  --kubeconfig=config \
   --server=$KUBERNETES_SERVER \
   --certificate-authority=cert.crt \
   --token=$KUBERNETES_TOKEN \
